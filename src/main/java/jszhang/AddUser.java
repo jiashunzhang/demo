412060d68d01e0b33aed148b5bec1302bf22beef
jszhang <jszhang@x230>
2020-04-25 10:11:57 +0800

    implement adduser

--- src/main/java/jszhang/AddUser.java
+++ src/main/java/jszhang/AddUser.java
@@ -2,6 +2,8 @@ package jszhang;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
 public class AddUser extends JDialog {
 
@@ -39,8 +41,84 @@ public class AddUser extends JDialog {
 
         l_name.setBounds(60, 20, 80, 40);
         l_name.setFont(kaiFont);
-        t_name.setBounds(150, 20, 80, 40);
+        t_name.setBounds(150, 20, 160, 40);
 
+        l_pass.setBounds(60, 80, 80, 40);
+        l_pass.setFont(kaiFont);
+        t_pass.setBounds(150, 80, 160, 40);
+
+        l_repass.setBounds(60, 140, 80, 40);
+        l_repass.setFont(kaiFont);
+        t_repass.setBounds(150, 140, 160, 40);
+
+        ok.setBounds(100, 200, 80, 40);
+        cancel.setBounds(240, 200, 80, 40);
+        ok.setFont(kaiFont);
+        cancel.setFont(kaiFont);
+
+        cancel.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddUser.this.dispose();
+            }
+
+        });
+
+        ok.addActionListener(new ActionListener(){
+
+            Connection con = null;
+            PreparedStatement statement = null;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String uname = t_name.getText();
+                String upass = new String(t_pass.getPassword());
+                String repass = new String(t_repass.getPassword());
+                if(uname.length() < 2) {
+                    JOptionPane.showMessageDialog(AddUser.this, "用户名太短！");
+                    return;
+                }
+                if(upass.length() < 2) {
+                    JOptionPane.showMessageDialog(AddUser.this, "too short");
+                    return;
+                }
+                if( !upass.equals(repass) ) {
+                    JOptionPane.showMessageDialog(AddUser.this, "password and confirm password should be the same");
+                    return;
+                }
+                //后面连接数据库
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    // JOptionPane.showMessageDialog(Login.this, "驱动加载成功");
+                    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo?useSSL=false&serverTimezone=UTC","sa","nicai");
+                    //JOptionPane.showMessageDialog(Login.this, "数据库连接成功");
+                    statement = con.prepareStatement("insert into users values(null, ?,?)");
+                    statement.setString(1, uname);
+                    statement.setString(2, upass);
+                    int result = statement.executeUpdate();
+                    if(result > 0){
+                        JOptionPane.showMessageDialog(AddUser.this, "添加成功！");
+                        AddUser.this.dispose();
+                    } else {
+                            JOptionPane.showMessageDialog(AddUser.this, "添加失败");
+                    }
+                    if(statement != null) {
+                        statement.close();
+                    }
+                    if(con != null) {
+                        con.close();
+                    }
+                } catch (ClassNotFoundException e1) {
+                    JOptionPane.showMessageDialog(AddUser.this, "驱动加载失败");
+                } catch (SQLException e1) {
+                    JOptionPane.showMessageDialog(AddUser.this, "添加失败");
+                    e1.printStackTrace();
+                }
+
+            }
+
+        });
 
     }
 
